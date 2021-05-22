8da24cce3b368e44818d6757253b2b143b13cbc8
rodrimadrid <rodri.fm94@gmail.com>
2021-05-22 00:03:36 -0300

    terminada

--- js/main.js
+++ js/main.js
@@ -1,32 +1,33 @@
+const reseñas = [{
+
+}]
+
+
 $(".formulario").submit((e)=>{
   e.preventDefault()
   alert('Nos vemos pronto!')
 })
 let positionReseña = $("#reseñas").scrollTop()
-
 $(document).scroll(function(){
 
   let position = $(document).scrollTop();
   let positionReseña = $("#reseñas").offset().top
   console.log(position)
-  if (position >= (positionReseña - 200)) {
-    $("#reseñasBox").css("left", "35px")
+  if ((position >= (positionReseña - 200))){
+    $("#1.reseña").css({left: "0", visibility: "visible"})
   }
 })
-let resPosition = 45
-let res = positionReseña - 200
 $("#btnRight").click((e)=>{
-  if (resPosition > -777) {
-    resPosition -= 274
-    $(".resBox").css({ left: `${resPosition}px` })
+  for (var i = 0; i < reseñas.length; i++) {
+    var visibility = document.getElementById(i).style.visibility
+    if (visibility == "visible") {
+      $('#'+i).css({left: "300px", visibility: "hidden"})
+    }
   }
 
 })
-$("#btnLeft").click((e)=>{
-  if (resPosition < 45) {
-    resPosition += 274
-    $(".resBox").css({ left: `${resPosition}px` })
-
+/*$("#btnLeft").click((e)=>{
+  if () {
   }
 
-})
+})*/
